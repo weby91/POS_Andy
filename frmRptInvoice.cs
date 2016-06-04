686ee94038179ce44089175db96086a065d7f05d
Webster <lebronwebz@gmail.com>
2016-06-05 01:39:24 +0700

    commit

---
+++ frmRptInvoice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_Andy
+{
+    public partial class frmRptInvoice : Form
+    {
+        public frmRptInvoice()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRptInvoice_Load(object sender, EventArgs e)
+        {
+            //rptInvoice invoiceReport = new rptInvoice();
+            //crystalReportViewer1.ReportSource = invoiceReport;
+        }
+    }
+}
